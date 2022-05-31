e554e6a3f3470821bf1dc89b8dab079f12553652
Victor Guthierry Dias da Silva <victor.guthierry.dias@gmail.com>
2022-05-31 00:49:05 -0300

    Create React Router

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+
+import { Link } from 'react-router-dom'; 
+import ButtonAppBar from './components/ButtonAppBar';
+import MultiActionAreaCard from './components/MultiActionAreaCard';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+    <div className='Pai'>
+
+      <ButtonAppBar></ButtonAppBar>
+      
+      <div className="Card">
+        <MultiActionAreaCard className="CardItem"></MultiActionAreaCard>
+        <MultiActionAreaCard className="CardItem"></MultiActionAreaCard>
+        <MultiActionAreaCard className="CardItem"></MultiActionAreaCard>
+      </div>
+
+      <div className="Card">
+        <MultiActionAreaCard className="CardItem"></MultiActionAreaCard>
+        <MultiActionAreaCard className="CardItem"></MultiActionAreaCard>
+        <MultiActionAreaCard className="CardItem"></MultiActionAreaCard>
+      </div>
+
+      <div>
+        <Link to="/login">Página de Login</Link> 
+      </div>
+      
     </div>
+    
   );
 }
 
